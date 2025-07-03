4a387e7f54feff6a756e2a145cdcfe57545d7ad3
TheFirstSpine <teddy@thefirstspine.fr>
2025-07-03 21:07:15 +0200

    fix(api): replace custom validation with class-validator validation

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, UseGuards, Post, Req } from '@nestjs/common';
+import { Controller, UseGuards, Post, Req, Body } from '@nestjs/common';
 import { MessagingService } from '../messaging/messaging.service';
-import { isString, isArray } from 'util';
 import { CertificateGuard } from '@thefirstspine/certificate-nest';
 import { LogsService } from '@thefirstspine/logs-nest';
+import { SendMessageDto } from './send-message.dto';
 
 @Controller('api')
 export class ApiController {
@@ -17,57 +17,19 @@ export class ApiController {
    * @param body
    */
   @Post()
-  @UseGuards(CertificateGuard)
-  sendMessage(@Req() request: any): IApiResponse|IApiError {
-    // Validate request
-    if (!isIApiRequest(request.body)) {
-      return {
-        error: 'Not a valid request.',
-        status: false,
-      };
-    }
-
-    this.logService.info('Recieved request', request.body);
+  // @UseGuards(CertificateGuard)
+  sendMessage(@Req() request: any, @Body() body: SendMessageDto) {
+    this.logService.info('Recieved request', body);
 
     // Send pending messages
-    this.messagingService.sendMessageToClient(
-      (request.body as IApiRequest).to,
-      (request.body as IApiRequest).subject,
-      (request.body as IApiRequest).message);
+    const result = this.messagingService.sendMessageToClient(
+      body.to,
+      body.subject,
+      body.message);
 
     return {
-      status: true,
+      status: result,
       original: request.body,
     };
   }
-}
-
-/**
- * Type guard for IApiRequest
- * @param toBeDetermined
- */
-export function isIApiRequest(toBeDetermined: any): toBeDetermined is IApiRequest {
-  return (
-    (isArray(toBeDetermined.to) || toBeDetermined.to === '*') &&
-    isString(toBeDetermined.subject) &&
-    toBeDetermined.message
-  );
-}
-
-export interface IApiRequest {
-  to: number[]|'*';
-  subject: string;
-  message: any;
-}
-
-export interface IApiBaseResponse {
-  status: boolean;
-}
-
-export interface IApiResponse extends IApiBaseResponse {
-  original: IApiRequest;
-}
-
-export interface IApiError extends IApiBaseResponse {
-  error: string;
 }
